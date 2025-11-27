39dac25443ba212f504b90ea26030756330d7337
Riggen <riggen.snyder@gmail.com>
2025-11-27 11:16:34 -0600

    added needed routes for events_controller.rb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   before_action :require_login
-
+  before_action :set_event, only: [:edit, :update, :destroy]
   def index
     @events = current_user.events
   end
@@ -18,7 +18,27 @@ class EventsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to events_path, notice: 'Event was successfully updated.'
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to events_path, notice: "Event deleted"
+  end
+
   private
+
+  def set_event
+    @event = current_user.events.find(params[:id])
+  end
 
   def event_params
     params.require(:event).permit(:name, :date, :description)
