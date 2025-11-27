4b865a1d0aa7e88d172d826f59011690fd67e2b9
Bella Conrad <isabella-conrad@uiowa.edu>
2025-11-27 08:52:59 -0600

    Delete unused functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,20 +13,6 @@ class UsersController < ApplicationController
   # 2. Triggers Model validations via @user.save.
   # 3. If successful: specific controller logic logs them in immediately (setting session).
   # 4. If failed: re-renders the 'new' View so errors can be displayed.
-
-  # OLD CODE BEFORE PROFILE INTEGRATION ==========================================================
-  # def create
-  #   @user = User.new(user_params_signup)
-  #   if @user.save
-  #     #redirect_to @user, notice: "Welcome, #{@user.email}"
-  #     session[:user_id] = @user.id
-  #     redirect_to root_path, notice: "Welcome, #{@user.email}"
-  #   else
-  #     flash.now[:alert] = "There was an error creating your account."
-  #     render :new, status: :unprocessable_entity
-  #   end
-  # end
-
   def create
     @user = User.new(user_params)
 
@@ -44,17 +30,6 @@ class UsersController < ApplicationController
   end
 
 
-  # DELETE THIS ACTION BEFORE PULL REQUEST FOR PROFILES ==============================================
-
-  # [MODEL & VIEW] Fetches data to display a user's profile.
-  # Queries the User model using the ID stored in the session (security check)
-  # and makes the @user variable available to the 'show' View.
-  # def show
-  #   # Show user details here
-  #   @user = current_user
-  # end
-  #
-
   private
 
   # [CONTROLLER] Strong Parameters pattern.
@@ -63,19 +38,6 @@ class UsersController < ApplicationController
   # Used for signup form
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
-  end
-
-
-  # DELETE THIS AND ADD TO PROFILE CONTROLLER BEFORE PULL REQUEST FOR PROFILES =======================
-
-
-  # [CONTROLLER] Strong Parameters for profile updates.
-  # Allows a different set of attributes to be modified than during signup.
-  # Used when editing the user's profile later
-  def user_params_profile
-    params.require(:user).permit(
-    :name, :age, :occupation, :hobbies, :likes, :dislikes
-    )
   end
 
 end
