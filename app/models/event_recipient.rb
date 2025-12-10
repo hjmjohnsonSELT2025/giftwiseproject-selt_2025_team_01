80c9947149d1433549d2bc7ecef04e4db24b81c7
Nick Mathews <nicholas-mathews@uiowa.edu>
2025-12-10 03:02:57 -0600

    event_recipient_gift_ideas creation migration & made url_validatable.rb - made migration to have event-specific gifts - url_validatable.rb is just a DRY helper thing for the url-validation for both event-specific & event-independent gifts

--- app/models/event_recipient.rb
+++ app/models/event_recipient.rb
@@ -9,4 +9,9 @@ class EventRecipient < ApplicationRecord
   # Prevents assigning the same recipient to the same event more than once.
   # Enforces that each (event_id, recipient_id) pair is unique.
   validates :event_id, uniqueness: { scope: :recipient_id }
+
+  # Each EventRecipient can have many event-specific gift ideas
+  # dependent: :destroy - if EventRecipient is destroyed, all associated event_recipient_gift_ideas will also
+  #   be destroyed to prevent orphaned data
+  has_many :event_recipient_gift_ideas, dependent: :destroy
 end
