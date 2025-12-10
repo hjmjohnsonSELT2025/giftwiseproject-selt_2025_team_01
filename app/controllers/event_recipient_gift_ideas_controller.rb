e80b5fad67a8509d106277c54cffc0702610af68
Nick Mathews <nicholas-mathews@uiowa.edu>
2025-12-10 05:07:34 -0600

    add & edit working

--- app/controllers/event_recipient_gift_ideas_controller.rb
+++ app/controllers/event_recipient_gift_ideas_controller.rb
@@ -21,7 +21,7 @@ class EventRecipientGiftIdeasController < ApplicationController
 
     if @gift_idea.save
       flash[:notice] = "Gift idea created."
-      redirect_to event_recipient_gift_ideas_path(@event, @recipient)
+      redirect_to event_path(@event)
     else
       render :new, status: :unprocessable_entity
     end
@@ -35,7 +35,7 @@ class EventRecipientGiftIdeasController < ApplicationController
   def update
     if @gift_idea.update(gift_idea_params)
       flash[:notice] = "Gift idea updated."
-      redirect_to event_recipient_gift_ideas_path(@event, @recipient)
+      redirect_to event_path(@event)
     else
       render :edit, status: :unprocessable_entity
     end
@@ -45,7 +45,7 @@ class EventRecipientGiftIdeasController < ApplicationController
   def destroy
     @gift_idea.destroy
     flash[:notice] = "Gift idea deleted."
-    redirect_to event_recipient_gift_ideas_path(@event, @recipient)
+    redirect_to event_path(@event)
   end
 
   private
@@ -67,7 +67,7 @@ class EventRecipientGiftIdeasController < ApplicationController
 
   def set_gift_idea
     @gift_idea = @event_recipient.event_recipient_gift_ideas.find_by(id: params[:id])
-    redirect_to event_recipient_gift_ideas_path(@event, @recipient), alert: "Gift idea not found." unless @gift_idea
+    redirect_to event_path(@event), alert: "Gift idea not found." unless @gift_idea
   end
 
   def gift_idea_params
