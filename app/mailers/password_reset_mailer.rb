a30bc3d3f7119f3d25402fad5ec603d1ff1340fd
SethB55 <sebolen@uiowa.edu>
2025-12-10 12:51:12 -0600

    Sanitize mail headers to fix CR/LF error

--- app/mailers/password_reset_mailer.rb
+++ app/mailers/password_reset_mailer.rb
@@ -1,10 +1,12 @@
 class PasswordResetMailer < ApplicationMailer
-  # Sends a password reset email containing a link with the raw token.
   def reset_email
     @user  = params[:user]
     token  = params[:token]
     @reset_url = edit_password_reset_url(token)
 
-    mail to: @user.email, subject: "Reset Password Link for your GiftWise Account"
+    mail(
+      to: @user.email.to_s.strip,
+      subject: "Reset your GiftWise password"
+    )
   end
 end
