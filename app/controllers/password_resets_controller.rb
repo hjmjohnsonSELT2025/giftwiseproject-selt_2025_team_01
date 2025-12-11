eaa205c85f301ffdbca841d6532146355d34ed10
SethB55 <sebolen@uiowa.edu>
2025-12-10 18:23:09 -0600

    ts aint working

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,14 +1,13 @@
 class PasswordResetsController < ApplicationController
-  # Let anyone access password reset flow
-  skip_before_action :require_login, only: %i[new create edit update]
+  # Password reset should work even if user is logged out and has no profile
+  skip_before_action :require_login
+  skip_before_action :ensure_profile_exists
 
   # GET /password_resets/new
-  # "Forgot your password?" page
   def new
   end
 
   # POST /password_resets
-  # Handle email form, send reset link if user exists
   def create
     if (user = User.find_by(email: params[:email]))
       token = user.generate_reset_password_token!
@@ -19,11 +18,11 @@ class PasswordResetsController < ApplicationController
   end
 
   # GET /password_resets/:id/edit
-  # Show "enter new password" form
   def edit
-    @user = User.find_by(reset_password_token: params[:id])
+    @token = params[:id]
+    @user  = User.find_by(reset_password_token: @token)
 
-    Rails.logger.info "DEBUG PASSWORD RESET: token=#{params[:id]} user_id=#{@user&.id}"
+    Rails.logger.info "DEBUG RESET EDIT: token=#{@token.inspect} user_id=#{@user&.id}"
 
     if @user.nil?
       flash[:alert] = "This reset link is invalid. Please request a new one."
@@ -31,31 +30,18 @@ class PasswordResetsController < ApplicationController
       return
     end
 
-
-    # Temporarily ignore expiry for debugging:
-    # if @user.reset_password_token_expired?
-    #   flash[:alert] = "This reset link has expired. Please request a new one."
-    #   redirect_to new_password_reset_path
-    #   return
-    # end
-
-    # If we get here, @user is set and we will render app/views/password_resets/edit.html.erb
+    # Renders app/views/password_resets/edit.html.erb
   end
 
   # PATCH /password_resets/:id
   def update
-    @user = User.find_by(reset_password_token: params[:id])
+    @token = params[:id]
+    @user  = User.find_by(reset_password_token: @token)
 
-    unless @user
+    if @user.nil?
       redirect_to new_password_reset_path, alert: "Invalid reset link."
       return
     end
-
-    # Temporarily ignore expiry while debugging:
-    # if @user.reset_password_token_expired?
-    #   redirect_to new_password_reset_path, alert: "This reset link has expired. Please request a new one."
-    #   return
-    # end
 
     new_password = params.dig(:user, :password)
 
