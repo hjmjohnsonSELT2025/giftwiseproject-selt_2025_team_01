05a84236d8e71f7cfd912dd95b3cba5fd0a0187b
Bella Conrad <isabella-conrad@uiowa.edu>
2025-12-12 19:42:32 -0600

    Try adding a safe list

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,21 @@ export default {
         "./app/components/**/*.erb",
         "./app/assets/stylesheets/**/*.css"
     ],
+    safelist: [
+        // Example: add all dynamic colors or classes here
+        'bg-red-500',
+        'bg-green-500',
+        'bg-blue-500',
+        'text-red-500',
+        'text-green-500',
+        'text-blue-500',
+        'hover:bg-red-500',
+        'hover:bg-green-500',
+        'hover:bg-blue-500',
+        'text-white',
+        'text-black',
+        // Add any other dynamic classes you use
+    ],
     theme: {
         extend: {},
     },
