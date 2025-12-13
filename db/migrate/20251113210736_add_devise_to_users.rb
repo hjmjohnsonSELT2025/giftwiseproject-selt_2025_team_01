52bbf5503dc571f53000b7ea52e1c48215c91e07
SethB55 <sebolen@uiowa.edu>
2025-12-13 15:57:14 -0600

    Make AddDeviseToUsers migration safe on existing schema

--- db/migrate/20251113210736_add_devise_to_users.rb
+++ db/migrate/20251113210736_add_devise_to_users.rb
@@ -4,15 +4,15 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.1]
   def self.up
     change_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false, default: "" unless column_exists?(:users, :email)
+      t.string :encrypted_password, null: false, default: "" unless column_exists?(:users, :encrypted_password)
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      t.string   :reset_password_token     unless column_exists?(:users, :reset_password_token)
+      t.datetime :reset_password_sent_at   unless column_exists?(:users, :reset_password_sent_at)
 
       ## Rememberable
-      t.datetime :remember_created_at
+      t.datetime :remember_created_at      unless column_exists?(:users, :remember_created_at)
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
@@ -37,15 +37,11 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.1]
       # t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    add_index :users, :email, unique: true unless index_exists?(:users, :email, unique: true)
+    add_index :users, :reset_password_token, unique: true unless index_exists?(:users, :reset_password_token, unique: true)
   end
 
   def self.down
-    # By default, we don't want to make any assumption about how to roll back a migration when your
-    # model already existed. Please edit below which fields you would like to remove in this migration.
     raise ActiveRecord::IrreversibleMigration
   end
 end
