f61c1a796c0b7ed07245fb3ee4ad274437612b23
Nick Mathews <nicholas-mathews@uiowa.edu>
2025-12-13 17:59:12 -0600

    spec/features/events_spec.rb passing

--- app/components/event_recipient_card_component.rb
+++ app/components/event_recipient_card_component.rb
@@ -1,7 +1,10 @@
 class EventRecipientCardComponent < ViewComponent::Base
-  def initialize(event:, recipient:)
+  attr_reader :event, :recipient, :event_recipient, :action
+
+  def initialize(event:, recipient:, action: :remove)
     @event = event
     @recipient = recipient
     @event_recipient = recipient.event_recipient_for(event)
+    @action = action
   end
 end
