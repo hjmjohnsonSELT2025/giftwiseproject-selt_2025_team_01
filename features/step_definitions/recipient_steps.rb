1e84727b4c9fc1afe11b59e3fec01dbe5b095f3b
Nick Mathews <nicholas-mathews@uiowa.edu>
2025-12-13 19:26:04 -0600

    features/ai_gift_suggestions.feature passing

--- features/step_definitions/recipient_steps.rb
+++ features/step_definitions/recipient_steps.rb
@@ -10,3 +10,8 @@ Given('a recipient exists with name {string} for user {string}') do |name, email
   user = User.find_by!(email: email)
   Recipient.create!(user: user, name: name)
 end
+
+Then('I should be on the recipient page for {string}') do |name|
+  recipient = Recipient.find_by!(name: name)
+  expect(page.current_path).to eq(recipient_path(recipient))
+end
